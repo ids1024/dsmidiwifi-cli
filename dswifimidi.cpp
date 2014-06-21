eca0cc29b909d4afc6399440af7db0cd5564823c
Ian Scott <ian@perebruin.com>
2014-06-20 21:30:10 -0700

    Remove QCoreApplication

--- dswifimidi.cpp
+++ dswifimidi.cpp
@@ -1,14 +1,11 @@
 #include <pthread.h>
 
-#include <QCoreApplication>
-
 #include "midi2udpthread.h"
 #include "udp2midithread.h"
 
 
-int main(int argc, char *argv[])
+int main()
 {
-	QCoreApplication app(argc, argv);
 	pthread_t midi2udpthread;
 	pthread_t udp2midithread;
 
@@ -18,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("midi2udp initialized\n");
 	} else {
 		fprintf(stderr, "Error initializing midi2udp!");
-		exit(1);
+		return 1;
 	}
 	
 	res = udp2midi_init();
@@ -27,8 +24,10 @@ int main(int argc, char *argv[])
 	pthread_create(&udp2midithread, NULL, udp2midithread_run, NULL);
 	} else {
 		fprintf(stderr, "Error initializing udp2midi!");
-		exit(1);
+		return 1;
 	}
 
-	return app.exec();
+	pthread_join(midi2udpthread, NULL);
+	pthread_join(udp2midithread, NULL);
+	return 0;
 }
